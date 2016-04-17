9882534e47577a178652b4f9e32c6d5bb7229e81
James Baicoianu <james_github@baicoianu.com>
2016-04-16 23:57:19 -0700

    Added remoteAddress/localAddress and related fields

--- src/WebSocketStream.js
+++ src/WebSocketStream.js
@@ -6,6 +6,13 @@ function WebSocketStream(driver, socket) {
   this._driver = driver;
   this._socket = socket;
   this.readable = this.writable = true;
+
+  this.localAddress = socket.localAddress;
+  this.localPort = socket.localPort;
+
+  this.remoteFamily = socket.remoteFamily;
+  this.remoteAddress = socket.remoteAddress;
+  this.remotePort = socket.remotePort;
   
   this._driver.on('message', function(message) { 
     this.emit('data', message.data);
