657eb6f66da94ebb63fc96c0a6fde63744434e27
Alonzo Coeus <alonzo.coeus@mail.com>
2019-01-05 19:44:38 +0000

    Moving to ES6
    Moving to ES6. Switching to normal class statements

--- src/Plugins.js
+++ src/Plugins.js
@@ -2,66 +2,73 @@ var args = require('optimist').argv;
 var config = require(args.config || '../config.js');
 var Session = require('./Session');
 
-function Plugins(server) {
-    this._plugins = [];
-    this._intervals = [];
-    this._methodPlugins = [];
-    this._server = server;
+class Plugins
+{
+    constructor(server)
+    {
+        this._plugins = [];
+        this._intervals = [];
+        this._methodPlugins = [];
+        this._server = server;
 
-    this.intervalLoad();
-    this.methodLoad();
-}
-
-Plugins.prototype.intervalLoad = function() {
-    var plugins = config.intervalPlugins;
-
-    for(var p in plugins) {
-        var file = plugins[p].plugin;
-        var plugin = require(file);
-        var interval = new plugin(this._server);
-
-        this._intervals[p] = setInterval(function() { interval.call(this._server); }, (plugins[p].interval * 1000)); 
-    }
-}
-
-Plugins.prototype.methodLoad = function() {
-    var plugins = config.methodPlugins;
-
-    for(var method in plugins) {
-        var file = plugins[method].plugin;
-        var plugin = require(file); 
-        Session.validMethods.push(method);
-        Session.prototype[method] = plugin.call;
-    }
-}
-
-Plugins.prototype.call = function(name, socket, command) {
-  var hook = config.hookPlugins[name];
-
-  for(var k in hook.plugins) {
-    var p = hook.plugins[k];
-
-    if(p === undefined) {
-        err = "Unable to load plugin " + p;
-        log.info(err);
-        console.log(err);
-        return;
-    } 
-
-    if(this._plugins[name] === undefined) {
-        this._plugins[name] = Array();
-    }
- 
-    if(this._plugins[name][p] === undefined) {
-        this.load(name, p);
+        this.intervalLoad();
+        this.methodLoad();
     }
 
-    this._plugins[name][p].call(name, socket, command);
-  }
-}
+    intervalLoad()
+    {
+        var plugins = config.intervalPlugins;
 
-Plugins.prototype.load = function(name, file) {
-    this._plugins[name][file] = require(file);
+        for(var p in plugins) {
+            var file = plugins[p].plugin;
+            var plugin = require(file);
+            var interval = new plugin(this._server);
+    
+            this._intervals[p] = setInterval(function() { interval.call(this._server); }, (plugins[p].interval * 1000)); 
+        }
+    }
+
+    methodLoad()
+    {
+        var plugins = config.methodPlugins;
+
+        for(var method in plugins) {
+            var file = plugins[method].plugin;
+            var plugin = require(file); 
+            Session.validMethods.push(method);
+            Session.prototype[method] = plugin.call;
+        }
+    }
+
+    call()
+    {
+        var hook = config.hookPlugins[name];
+
+        for(var k in hook.plugins) {
+            var p = hook.plugins[k];
+            
+            if(p === undefined) {
+                err = "Unable to load plugin " + p;
+                log.info(err);
+                console.log(err);
+                return;
+            } 
+
+            if(this._plugins[name] === undefined) {
+                this._plugins[name] = Array();
+            }
+       
+            if(this._plugins[name][p] === undefined) {
+                this.load(name, p);
+            }
+            this._plugins[name][p].call(name, socket, command);
+        }
+    }
+
+    load(name, file)
+    {
+        this._plugins[name][file] = require(file);
+    }
 }
 
 module.exports = Plugins;
--- src/WebSocketStream.js
+++ src/WebSocketStream.js
@@ -1,35 +1,40 @@
 var Stream = require('stream'),
     util = require('util');
 
-function WebSocketStream(driver, socket) {
-  Stream.call(this);
-  this.destroyed = false;
-  this._driver = driver;
-  this._socket = socket;
-  this.readable = this.writable = true;
-
-  this.localAddress = socket.localAddress;
-  this.localPort = socket.localPort;
-
-  this.remoteFamily = socket.remoteFamily;
-  this.remoteAddress = socket.remoteAddress;
-  this.remotePort = socket.remotePort;
+class WebSocketStream
+{
+  constructor(driver, socket)
+  {
+    Stream.call(this);
+    this.destroyed = false;
+    this._driver = driver;
+    this._socket = socket;
+    this.readable = this.writable = true;
   
-  this._driver.on('message', function(message) { 
-    this.emit('data', message.data);
-  }.bind(this));
+    this.localAddress = socket.localAddress;
+    this.localPort = socket.localPort;
+  
+    this.remoteFamily = socket.remoteFamily;
+    this.remoteAddress = socket.remoteAddress;
+    this.remotePort = socket.remotePort;
+    
+    this._driver.on('message', function(message) { 
+      this.emit('data', message.data);
+    }.bind(this));
+  
+  
+    this._driver.on('close', function() {
+      this.emit('close');
+      this.destroyed = true;
+    }.bind(this));
+  }
 
-
-  this._driver.on('close', function() {
-    this.emit('close');
-    this.destroyed = true;
-  }.bind(this));
+  write(message)
+  {
+    this._driver.text(message);
+  }
 }
 
 util.inherits(WebSocketStream, Stream);
 
 module.exports = WebSocketStream;
-
-WebSocketStream.prototype.write = function(message) {
-  this._driver.text(message);
-}
